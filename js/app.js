77bf628cb7a016412144bc92435ab8232da294b9
Neven Bojkovic <nev.enbo@gmail.com>
2020-04-24 20:44:29 +0200

    Final

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    dots:false,
-    autoplay:true,
-    autoplayTimeout:2500,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 1
         },
-        1000:{
-            items:1
+        1000: {
+            items: 1
         }
     }
 });
